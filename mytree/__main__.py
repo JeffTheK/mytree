d9b18cc1bec359799d8139889376e4278da9403c
JeffTheK <jeff144@protonmail.com>
2021-12-12 17:08:44 +0200

    Add dirs to ignore

--- mytree/__main__.py
+++ mytree/__main__.py
@@ -1,6 +1,10 @@
 import os
 import glob
 
+dirs_to_ignore = [
+    ".git"
+]
+
 def main():
     print('.')
     print(os.listdir('./mytree'))
@@ -11,8 +15,7 @@ def print_dir(path, padding=0):
     for s in stuff:
         if os.path.isfile(f"{path}/{s}"):
             print(' ' * padding + '├──' + s)
-        elif os.path.isdir(f"{path}/{s}"):
+        elif os.path.isdir(f"{path}/{s}") and not s in dirs_to_ignore:
             print(' ' * padding + '├──' + s)
-            print(f"{path}/{s}")
             print_dir(f"{path}/{s}", padding + 2)
         
