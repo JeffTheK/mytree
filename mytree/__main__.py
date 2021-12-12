46d82b35079ab22e584e1124365ed2d5c5774b03
JeffTheK <jeff144@protonmail.com>
2021-12-12 17:04:03 +0200

    Add basic tree printing

--- mytree/__main__.py
+++ mytree/__main__.py
@@ -1,2 +1,18 @@
+import os
+import glob
+
 def main():
-    print("hello world")
+    print('.')
+    print(os.listdir('./mytree'))
+    print_dir('.')
+
+def print_dir(path, padding=0):
+    stuff = os.listdir(path)
+    for s in stuff:
+        if os.path.isfile(f"{path}/{s}"):
+            print(' ' * padding + '├──' + s)
+        elif os.path.isdir(f"{path}/{s}"):
+            print(' ' * padding + '├──' + s)
+            print(f"{path}/{s}")
+            print_dir(f"{path}/{s}", padding + 2)
+        
