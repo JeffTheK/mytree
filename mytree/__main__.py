3fc7123df4eab49004b21470fed3acc037fc0da6
JeffTheK <jeff144@protonmail.com>
2021-12-12 17:11:45 +0200

    Print dirs first

--- mytree/__main__.py
+++ mytree/__main__.py
@@ -7,15 +7,15 @@ dirs_to_ignore = [
 
 def main():
     print('.')
-    print(os.listdir('./mytree'))
     print_dir('.')
 
 def print_dir(path, padding=0):
     stuff = os.listdir(path)
+    stuff.sort(key=lambda s: os.path.isfile(s))
     for s in stuff:
         if os.path.isfile(f"{path}/{s}"):
             print(' ' * padding + '├──' + s)
         elif os.path.isdir(f"{path}/{s}") and not s in dirs_to_ignore:
             print(' ' * padding + '├──' + s)
-            print_dir(f"{path}/{s}", padding + 2)
+            print_dir(f"{path}/{s}", padding + 3)
         
