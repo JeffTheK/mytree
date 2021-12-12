6b9117f67922f4f6a339d09b079b3ca581e7b3e6
JeffTheK <jeff144@protonmail.com>
2021-12-12 17:20:00 +0200

    Add last file symbol

--- mytree/__main__.py
+++ mytree/__main__.py
@@ -12,10 +12,16 @@ def main():
 def print_dir(path, padding=0):
     stuff = os.listdir(path)
     stuff.sort(key=lambda s: os.path.isfile(s))
-    for s in stuff:
+    symbol = '├──'
+    for i, s in enumerate(stuff, start=1):
+        if i == len(stuff):
+            symbol = '└──'
+        else:
+            symbol = '├──'
+            
         if os.path.isfile(f"{path}/{s}"):
-            print(' ' * padding + '├──' + s)
+            print(' ' * padding + symbol + s)
         elif os.path.isdir(f"{path}/{s}") and not s in dirs_to_ignore:
-            print(' ' * padding + '├──' + s)
+            print(' ' * padding + symbol + s)
             print_dir(f"{path}/{s}", padding + 3)
         
